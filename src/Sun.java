8cfc9011e0cc217802697415a1d167c5753233d2
fislhhhhh <faishal23firdaus@gmail.com>
2024-04-30 14:43:38 +0700

    sun

--- src/Sun.java
+++ src/Sun.java
@@ -2,6 +2,7 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
@@ -11,6 +12,8 @@ public class Sun extends Shapes implements CustomListener{
     int pointsun = 25;
     int intervalsun;
     boolean falling = false ;
+    Random acak = new Random();
+    
 
     protected Sun(int X ,int Y){
         super(X,Y);
@@ -18,6 +21,8 @@ public class Sun extends Shapes implements CustomListener{
 
     public void Start_falling(){
         falling=true;
+
+
     }
     public void Stop_falling(){
         falling=false;
@@ -31,7 +36,13 @@ public class Sun extends Shapes implements CustomListener{
         jumlahsun = 0;
     }
     public void actionPerformed() {
-    
+            int bebas = acak.nextInt(120,240);
+            while (bebas > 0){
+                Start_falling();
+                Y = Y -1;
+                bebas = bebas -1 ;
+            }
+            Stop_falling();
         }
     
     
@@ -39,3 +50,4 @@ public class Sun extends Shapes implements CustomListener{
 
 
 }
+
